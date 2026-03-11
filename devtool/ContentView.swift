47cfbe4d4b2be7da808d7666021805259c0ce3b1
ThangTQ <thang.to@golfzon.com>
2026-03-11 10:05:40 +0700

    Initial Commit

---
+++ devtool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  devtool
+//
+//  Created by GOLFZON on 11/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ devtool/devtoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  devtoolApp.swift
+//  devtool
+//
+//  Created by GOLFZON on 11/3/26.
+//
+
+import SwiftUI
+
+@main
+struct devtoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
